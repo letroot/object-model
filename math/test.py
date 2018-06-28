e166e452307504687adf753bbf023b54888f2692
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2018-06-28 08:36:16 +0100

    Add docstrings

--- math/test.py
+++ math/test.py
@@ -1,23 +1,35 @@
+"""Vector class test suite"""
+
 import unittest
-from vector import Vector
+
+from vectors import Vector
+
 
 class TestCase(unittest.TestCase):
 
     def test_equality(self):
-        assertEqual(Vector(2, 4), Vector(2, 4))
+        """Test equality."""
+        self.assertEqual(Vector(2, 4), Vector(2, 4))
 
     def test_addition(self):
-        sum = Vector(2, 3) + Vector(2, 3)
-        assertEqual(sum, Vector(4, 6))
+        """Test addition operations."""
+        sum_ = Vector(2, 3) + Vector(2, 3)
+        self.assertEqual(sum_, Vector(4, 6))
 
     def test_scalar_mul(self):
+        """Test scalar multiplication."""
         v1 = Vector(1, 9)
-        assertEqual(v1 * 3, Vector(3, 27))
+        self.assertEqual(v1 * 3, Vector(3, 27))
 
     def test_hypotenuse(self):
+        """Test resultant vector."""
         v1 = Vector(5, 12)
-        assertEqual(13, abs(v1))
-    
+        self.assertEqual(13, abs(v1))
+
     def test_null_vector(self):
+        """Zero vectors exist, you know"""
         v1 = Vector(0, 0)
-        assertFalse(bool(abs(v1)))
+        self.assertFalse(bool(v1))
+
+if __name__ == "__main__":
+    unittest.main()
